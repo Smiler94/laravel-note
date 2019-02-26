f90179acf9a99604bc35568875b26d1b2b0722ec
linzhen <273487984@qq.com>
2019-02-26 13:49:01 +0800

    notification

--- laravel-demo/app/Http/Controllers/TestController.php
+++ laravel-demo/app/Http/Controllers/TestController.php
@@ -10,6 +10,7 @@ class TestController extends Controller
     //
     public function test()
     {
-        LinzhenQueue::dispatch(['test'=>'test']);
+//        LinzhenQueue::dispatch(['test'=>'test']);
+        dump(app('queue')->connection('rabbitmq')->pushRaw(json_encode(['test'=>'test'])));
     }
 }
